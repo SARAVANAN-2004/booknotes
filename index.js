6d85b740b67ddf31ea89e2062123ad9ebca17b3f
Saravanan G <saravanansenthik@gmail.com>
2025-02-16 20:33:36 +0530

    completed details page

--- index.js
+++ index.js
@@ -20,17 +20,31 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 let items = [];
+let book = {};
 
 async function getItems(basedOn) {
     const res = await db.query("Select * from items order by $1",[basedOn]);
-    console.log(res.rows);
+    // console.log(res.rows);
     items = res.rows;
+}
+
+async function getbook(id) {
+    const res = await db.query("select * from items where id = $1",[id]);
+    console.log(res.rows);
+    book = res.rows[0];
+    book.my_notes = book.my_notes ? book.my_notes.split('\n') : [];
 }
 
 app.get("/", async (req,res)=>{
     await getItems("recomendation");
     res.render("index.ejs",{items:items});
 })
+
+app.post("/details",async (req,res)=>{
+    await getbook(req.body.id);
+    console.log(book);
+    res.render("details.ejs",{book:book});
+});
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
