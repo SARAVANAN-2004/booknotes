4352f06302b8faa6060926d60ce7d8c6b06e247c
Saravanan G <saravanansenthik@gmail.com>
2025-02-16 18:54:10 +0530

    connect to database and show in the home page

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ import ejs from 'ejs';
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"books",
+    password:"saravanan@28",
+    port:5432
+});
+
+db.connect();
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+let items = [];
 
-app.get("/",(req,res)=>{
+async function getItems(basedOn) {
+    const res = await db.query("Select * from items order by $1",[basedOn]);
+    console.log(res.rows);
+    items = res.rows;
+}
 
-    res.render("index.ejs");
+app.get("/", async (req,res)=>{
+    await getItems("recomendation");
+    res.render("index.ejs",{items:items});
 })
 
 app.listen(port, () => {
