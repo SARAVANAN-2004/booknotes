39adeaf2abd31efa4b0ee3c792f6fcc49808fefe
Saravanan G <saravanansenthik@gmail.com>
2025-02-16 21:35:36 +0530

    added sorting

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 let items = [];
 let book = {};
 
-async function getItems(basedOn) {
-    const res = await db.query("Select * from items order by $1",[basedOn]);
+async function getItems() {
+    const res = await db.query("select * from items order by recomendation desc;");
     // console.log(res.rows);
     items = res.rows;
 }
@@ -33,6 +33,23 @@ async function getbook(id) {
     console.log(res.rows);
     book = res.rows[0];
     book.my_notes = book.my_notes ? book.my_notes.split('\n') : [];
+}
+
+async function sortItem(criteria) {
+    console.log(criteria);
+    if (criteria === "title") {
+        const res = await db.query("select * from items order by title ;");
+        // console.log(res.rows);
+        items = res.rows;
+    }else if(criteria === "recomendation"){
+        const res = await db.query("Select * from items order by recomendation desc");
+        // console.log(res.rows);
+        items = res.rows;
+    }else{
+        const res = await db.query("Select * from items order by date desc");
+        // console.log(res.rows);
+        items = res.rows;
+    }
 }
 
 app.get("/", async (req,res)=>{
@@ -45,6 +62,13 @@ app.post("/details",async (req,res)=>{
     console.log(book);
     res.render("details.ejs",{book:book});
 });
+
+app.get("/sort",async (req,res)=>{
+    await sortItem(req.query.criteria);
+    // console.log(req.query.criteria);
+    // console.log(items);
+    res.render("index.ejs",{items:items});
+})
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
